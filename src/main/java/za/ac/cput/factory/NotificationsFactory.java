e8dfdce4e0f0830cc0f0427413fb06914c559863
Phumelela142 <240040546@mycput.ac.za>
2026-03-16 23:40:34 +0200

    Add createNotifications method to NotificationsFactory

--- src/main/java/za/ac/cput/factory/NotificationsFactory.java
+++ src/main/java/za/ac/cput/factory/NotificationsFactory.java
@@ -1,4 +1,25 @@
 package za.ac.cput.domain.factory;
+import za.ac.cput.domain.Notifications;
+import java.time.LocalDateTime;
 
 public class NotificationsFactory {
+
+    public static Notifications createNotifications(int notificationsID,
+                                                  String message,
+                                                  String notificationsType,
+                                                  int userID) {
+
+        if (message == null || message.isEmpty()) {
+            return null;
+        }
+
+        return new Notifications.Builder()
+                .setNotificationsID(notificationsID)
+                .setMessage(message)
+                .setNotificationsType(notificationsType)
+                .setDateSent(LocalDateTime.now())
+                .setStatus("Sent")
+                .setUserID(userID)
+                .build();
+    }
 }
