b35e578b49f301788603683ca19b75fb1f97bcf3
IngaPlati <230126634@mycput.ac.za>
2026-08-19 00:20:20 +0200

    Implement Race entity with Builder pattern

--- src/main/java/za/ac/cput/domain/Race.java
+++ src/main/java/za/ac/cput/domain/Race.java
@@ -1,4 +1,70 @@
+/* Race.java
+   Author: Inga Plati (230126634)
+   Date: 18 August 2026 */
 package za.ac.cput.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Race {
+
+    @Id
+    @Column(name = "race_id")
+    private String raceId;
+    private String description;
+
+    public Race() {
+    }
+
+    private Race(Builder builder) {
+        this.raceId = builder.raceId;
+        this.description = builder.description;
+    }
+
+    public String getRaceId() {
+        return raceId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Race{" +
+                "raceId='" + raceId + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String raceId;
+        private String description;
+
+        public Builder setRaceId(String raceId) {
+            this.raceId = raceId;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder copy(Race race) {
+            this.raceId = race.raceId;
+            this.description = race.description;
+            return this;
+        }
+
+        public Race build() {
+            return new Race(this);
+        }
+    }
 }
