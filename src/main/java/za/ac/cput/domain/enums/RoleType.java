4fbd24938fa3286c6dca184eca0ca8326b6ca604
shibambocollins <ntsobokwanec@gmail.com>
2026-08-18 10:33:36 +0200

    Add RoleType enumeration with MEMBER, TRAINER, and ADMIN values

--- src/main/java/za/ac/cput/domain/enums/RoleType.java
+++ src/main/java/za/ac/cput/domain/enums/RoleType.java
@@ -1,4 +1,12 @@
+/* RoleType.java
+     RoleType enumeration
+     Author: Collins Shibambo (230093183)
+     Date: 18 August 2026 */
 package za.ac.cput.domain.enums;
 
 public enum RoleType {
+    MEMBER,
+    TRAINER,
+    ADMIN
 }
+ 
