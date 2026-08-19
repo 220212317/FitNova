07550a06398d9ffc5e92bf8f9e216c7d925bb118
shibambocollins <ntsobokwanec@gmail.com>
2026-08-19 22:41:48 +0200

    Fix User entity relationship mappings; add UserFactory and UserRoleFactory

--- src/main/java/za/ac/cput/factory/UserRoleFactory.java
+++ src/main/java/za/ac/cput/factory/UserRoleFactory.java
@@ -1,4 +1,40 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.User;
+import za.ac.cput.domain.UserRole;
+import za.ac.cput.domain.enums.RoleType;
+import za.ac.cput.util.Helper;
+
+/*
+ * UserRoleFactory.java
+ * Author: Collins Shibambo
+ * 230093183
+ */
 public class UserRoleFactory {
+
+    public static UserRole createUserRole(String userRoleId, User user, RoleType roleId, String description) {
+
+        if (user == null) {
+            return null;
+        }
+
+        if (roleId == null) {
+            return null;
+        }
+
+        if (Helper.isNullOrEmpty(userRoleId)) {
+            userRoleId = Helper.generateId();
+        }
+
+        return new UserRole.Builder()
+                .setUserRoleId(userRoleId)
+                .setUser(user)
+                .setRoleId(roleId)
+                .setDescription(description)
+                .build();
+    }
+
+    public static UserRole createUserRole(User user, RoleType roleId, String description) {
+        return createUserRole(Helper.generateId(), user, roleId, description);
+    }
 }
