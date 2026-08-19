83dddc2cf61604fa0792a5ee554951205c7ceb6b
Lisakhanyatshokolo5 <lisakhanyatshokolo@gmail.com>
2026-08-19 18:37:34 +0200

    Add NextOfKinContactFactory

--- src/main/java/za/ac/cput/factory/NextOfKinContactFactory.java
+++ src/main/java/za/ac/cput/factory/NextOfKinContactFactory.java
@@ -1,4 +1,35 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.NextOfKinContact;
+import za.ac.cput.util.Helper;
+//Lisakhanya Tshokolo 220239215
+
 public class NextOfKinContactFactory {
+    public static NextOfKinContact createNextOfKinContact(
+                String firstName, String lastName, String relationship, String cellphoneNumber) {
+        if (Helper.isNullOrEmpty(firstName)) {
+                return null;
+        }
+
+        if (Helper.isNullOrEmpty(lastName)) {
+                return null;
+        }
+
+        if (Helper.isNullOrEmpty(relationship)) {
+                return null;
+        }
+        if (Helper.isNullOrEmpty(cellphoneNumber)) {
+                return null;
+        }
+
+        String nextofkinContactId = Helper.generateId();
+
+        return new NextOfKinContact.Builder()
+                    .setNextOfKinContactId(nextofkinContactId)
+                    .setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setRelationship(relationship)
+                    .setCellphoneNumber(cellphoneNumber)
+                    .build();
+    }
 }
