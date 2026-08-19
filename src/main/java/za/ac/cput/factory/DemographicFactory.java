5bfff90b30bcdc42d816792cddb6bed2b92801f6
IngaPlati <230126634@mycput.ac.za>
2026-08-19 23:29:38 +0200

    Implement DemographicFactory

--- src/main/java/za/ac/cput/factory/DemographicFactory.java
+++ src/main/java/za/ac/cput/factory/DemographicFactory.java
@@ -1,4 +1,32 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.Demographic;
+import za.ac.cput.domain.Gender;
+import za.ac.cput.domain.Race;
+import za.ac.cput.util.Helper;
+
+/*
+ * DemographicFactory.java
+ * Author: Inga Plati
+ * 230126634
+ */
 public class DemographicFactory {
+
+    public static Demographic createDemographic(String demographyId, Gender gender, Race race) {
+        if (gender == null || race == null) {
+            return null;
+        }
+        if (Helper.isNullOrEmpty(demographyId)) {
+            demographyId = Helper.generateId();
+        }
+        return new Demographic.Builder()
+                .setDemographyId(demographyId)
+                .setGender(gender)
+                .setRace(race)
+                .build();
+    }
+
+    public static Demographic createDemographic(Gender gender, Race race) {
+        return createDemographic(Helper.generateId(), gender, race);
+    }
 }
