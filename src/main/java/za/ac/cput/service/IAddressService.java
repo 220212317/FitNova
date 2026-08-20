4e3d2713b2ee8e3e5b901fc1a898e531b2b4b2d0
Athi Sintiya <220212317@mycput.ac.za>
2026-08-20 17:35:54 +0200

    implement IAddressService

--- src/main/java/za/ac/cput/service/IAddressService.java
+++ src/main/java/za/ac/cput/service/IAddressService.java
@@ -1,4 +1,19 @@
 package za.ac.cput.service;
 
-public interface IAddressService {
+import za.ac.cput.domain.Address;
+
+import java.util.List;
+
+/*
+ * Author: Athi Sintiya
+ * 220212317
+ */
+
+public interface IAddressService extends IService<Address, String> {
+
+    List<Address> findAddressByCity(String city);
+
+    List<Address> findAddressByProvince(String province);
+
+    List<Address> findAddressByPostalCode(String postalCode);
 }
