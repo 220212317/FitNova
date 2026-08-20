a1cc23a7ed60490986150ecfeadea089f09c655b
Athi Sintiya <220212317@mycput.ac.za>
2026-08-20 17:11:04 +0200

    implement IAccountRepository query methods

--- src/main/java/za/ac/cput/repository/IAccountRepository.java
+++ src/main/java/za/ac/cput/repository/IAccountRepository.java
@@ -1,7 +1,21 @@
 package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Account;
 
+import java.time.LocalDate;
+import java.util.List;
+
+/*
+ * Author: Athi Sintiya
+ * 220212317
+ */
+
+@Repository
 public interface IAccountRepository extends JpaRepository<Account, String> {
+
+    Account findByEmail(String email);
+
+    List<Account> findAccountByRegistrationDate(LocalDate registrationDate);
 }
