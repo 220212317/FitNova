737bf9e94cbf5d70d827124f9626fe92ea7a9ba5
Lisakhanyatshokolo5 <lisakhanyatshokolo@gmail.com>
2026-08-20 21:16:43 +0200

    Added nextofkincontact service

--- src/main/java/za/ac/cput/service/INextOfKinContactService.java
+++ src/main/java/za/ac/cput/service/INextOfKinContactService.java
@@ -1,4 +1,11 @@
 package za.ac.cput.service;
+import za.ac.cput.domain.NextOfKinContact;
+import java.util.Optional;
+//Lisakhanya Tshokolo 220239215
 
-public interface INextOfKinContactService {
+public interface INextOfKinContactService  extends IService<NextOfKinContact,String>{
+    Optional<NextOfKinContact> findByfirstName(String firstName);
+    Optional<NextOfKinContact> findBylastName(String lastName);
+    Optional<NextOfKinContact> findByrelationship(String relationship);
+    Optional<NextOfKinContact> findBycellphoneNumber(String cellphoneNumber);
 }
