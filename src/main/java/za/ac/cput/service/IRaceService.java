2235896e18262890957aadeef237947d6768644f
IngaPlati <230126634@mycput.ac.za>
2026-08-20 17:06:10 +0200

    Implement IRaceService

--- src/main/java/za/ac/cput/service/IRaceService.java
+++ src/main/java/za/ac/cput/service/IRaceService.java
@@ -1,4 +1,23 @@
+/*
+ * IRaceService.java
+ * Author: Inga Plati
+ * 230126634
+ */
 package za.ac.cput.service;
 
+import za.ac.cput.domain.Race;
+
+import java.util.List;
+
 public interface IRaceService {
+
+    Race create(Race race);
+
+    Race read(String raceId);
+
+    Race update(Race race);
+
+    void delete(String raceId);
+
+    List<Race> getAll();
 }
