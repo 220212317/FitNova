1d0a1f294761d8df544d1d6b02fd53dd1ff4cdab
IngaPlati <230126634@mycput.ac.za>
2026-08-20 16:14:56 +0200

    Implement IRaceRepository

--- src/main/java/za/ac/cput/repository/IRaceRepository.java
+++ src/main/java/za/ac/cput/repository/IRaceRepository.java
@@ -1,4 +1,12 @@
+/*
+ * IRaceRepository.java
+ * Author: Inga Plati
+ * 230126634
+ */
 package za.ac.cput.repository;
 
-public interface IRaceRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.Race;
+
+public interface IRaceRepository extends JpaRepository<Race, String> {
 }
