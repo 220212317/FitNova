36edeb64cde36d1c1a2a80594f1c2b492c1dbf24
Lisakhanyatshokolo5 <lisakhanyatshokolo@gmail.com>
2026-08-20 21:10:05 +0200

    Added Contact Repository

--- src/main/java/za/ac/cput/repository/IContactRepository.java
+++ src/main/java/za/ac/cput/repository/IContactRepository.java
@@ -1,4 +1,10 @@
 package za.ac.cput.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.Contact;
+import java.util.Optional;
+//Lisakhanya Tshokolo 220239215
 
-public interface IContactRepository {
+public interface IContactRepository extends JpaRepository<Contact, String> {
+    Optional<Contact> findByCellphoneNumber(String cellphoneNumber);
+    Optional<Contact> findByEmailAddress(String emailAddress);
 }
