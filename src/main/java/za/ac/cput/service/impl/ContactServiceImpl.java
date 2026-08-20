6ab34d59c04fe192f5d1a474b7b1d148074dafb7
Lisakhanyatshokolo5 <lisakhanyatshokolo@gmail.com>
2026-08-20 21:18:02 +0200

    Added Contact implement

--- src/main/java/za/ac/cput/service/impl/ContactServiceImpl.java
+++ src/main/java/za/ac/cput/service/impl/ContactServiceImpl.java
@@ -1,4 +1,61 @@
 package za.ac.cput.service.impl;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Contact;
+import za.ac.cput.repository.IContactRepository;
+import za.ac.cput.service.IContactService;
+import java.util.List;
+import java.util.Optional;
+//Lisakhanya Tshokolo 220239215
 
-public class ContactServiceImpl {
+@Service
+public class ContactServiceImpl implements IContactService {
+
+    private IContactRepository repository;
+
+    public ContactServiceImpl(IContactRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Contact create(Contact contact) {
+        return repository.save(contact);
+    }
+
+    @Override
+    public Contact read(String contactId) {
+        return repository.findById(contactId).orElse(null);
+    }
+
+    @Override
+    public Contact update(Contact contact) {
+        if(repository.existsById(contact.getContactId())){
+            return repository.save(contact);
+        }
+        return null;
+    }
+
+    @Override
+    public Optional<Contact> findByCellphoneNumber(String cellphoneNumber) {
+        return repository.findByCellphoneNumber(cellphoneNumber);
+    }
+
+    @Override
+    public Optional<Contact> findByEmailAddress(String emailAddress) {
+        return repository.findByEmailAddress(emailAddress);
+    }
+
+    @Override
+    public List<Contact> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public boolean delete(String contactId) {
+        if(repository.existsById(contactId)){
+            repository.deleteById(contactId);
+            return true;
+        }
+        return false;
+
+    }
 }
