a8b7b0ea96e51f60c947d04692d406be9e75b772
shibambocollins <ntsobokwanec@gmail.com>
2026-08-20 21:21:04 +0200

    Add IUservice interface and implement all query methods

--- src/main/java/za/ac/cput/service/IUserService.java
+++ src/main/java/za/ac/cput/service/IUserService.java
@@ -1,4 +1,17 @@
+/*
+ * IUserService.java
+ * Author: Collins Shibambo
+ * 230093183
+ */
 package za.ac.cput.service;
 
-public interface IUserService {
+import za.ac.cput.domain.User;
+
+import java.util.List;
+
+public interface IUserService extends IService<User, String> {
+
+    List<User> findByFirstNameAndLastName(String firstName, String lastName);
+
+    List<User> searchByLastName(String lastName);
 }
--- src/main/java/za/ac/cput/service/impl/UserServiceImpl.java
+++ src/main/java/za/ac/cput/service/impl/UserServiceImpl.java
@@ -1,4 +1,63 @@
+/*
+ * UserServiceImpl.java
+ * Author: Collins Shibambo
+ * 230093183
+ */
 package za.ac.cput.service.impl;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.User;
+import za.ac.cput.repository.IUserRepository;
+import za.ac.cput.service.IUserService;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements IUserService {
+
+    private final IUserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User read(String userId) {
+        return userRepository.findById(userId).orElse(null);
+    }
+
+    @Override
+    public User update(User user) {
+        if (!userRepository.existsById(user.getUserId())) {
+            return null;
+        }
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void delete(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public List<User> findByFirstNameAndLastName(String firstName, String lastName) {
+        return userRepository.findByFirstNameAndLastName(firstName, lastName);
+    }
+
+    @Override
+    public List<User> searchByLastName(String lastName) {
+        return userRepository.findByLastNameContainingIgnoreCase(lastName);
+    }
 }
