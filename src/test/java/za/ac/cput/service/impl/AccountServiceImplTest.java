3c5a05392f8db43f5c616d195734cb1c1748dd1d
Athi Sintiya <220212317@mycput.ac.za>
2026-08-21 16:39:40 +0200

    update: disable delete

--- src/test/java/za/ac/cput/service/impl/AccountServiceImplTest.java
+++ src/test/java/za/ac/cput/service/impl/AccountServiceImplTest.java
@@ -1,22 +1,15 @@
 package za.ac.cput.service.impl;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import za.ac.cput.domain.Account;
+import za.ac.cput.factory.AccountFactory;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-/*
- * Author: Athi Sintiya
- * 220212317
- */
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -25,81 +18,65 @@ class AccountServiceImplTest {
     @Autowired
     private AccountServiceImpl accountService;
 
-    private static final String ACCOUNT_ID = "ACC-001";
-    private static final String EMAIL = "athi.sintiya@test.com";
-    private static final LocalDate REG_DATE = LocalDate.of(2026, 8, 20);
-
-    private static final Account account = new Account.Builder()
-            .setAccountId(ACCOUNT_ID)
-            .setEmail(EMAIL)
-            .setPassword("SecurePass123")
-            .setRegistrationDate(REG_DATE)
-            .build();
+    private static Account account = AccountFactory.createAccount(
+            "ACC-001",
+            "athi.sintiya@test.com",
+            "SecurePass123",
+            LocalDate.of(2026, 8, 20)
+    );
 
     @Test
     @Order(1)
     void create() {
-        Account created = accountService.create(account);
-        assertNotNull(created);
-        assertEquals(account.getAccountId(), created.getAccountId());
-        System.out.println("Created: " + created);
+        Account account1 = accountService.create(account);
+        assertNotNull(account1);
+        System.out.println(account1);
     }
 
     @Test
     @Order(2)
     void read() {
-        Account read = accountService.read(account.getAccountId());
-        assertNotNull(read);
-        assertEquals(account.getEmail(), read.getEmail());
-        System.out.println("Read: " + read);
+        Account account1 = accountService.read(account.getAccountId());
+        System.out.println(account1);
     }
 
     @Test
     @Order(3)
     void update() {
-        Account updatedAccount = new Account.Builder()
-                .copy(account)
+        Account account1 = new Account.Builder().copy(account)
                 .setPassword("NewSecurePass456")
                 .build();
-        Account updated = accountService.update(updatedAccount);
-        assertNotNull(updated);
-        assertEquals("NewSecurePass456", updated.getPassword());
-        System.out.println("Updated: " + updated);
+
+        Account updatedAccount = accountService.update(account1);
+        System.out.println(updatedAccount);
     }
 
     @Test
-    @Order(4)
-    void findByEmail() {
-        Account found = accountService.findByEmail(EMAIL);
-        assertNotNull(found);
-        assertEquals(EMAIL, found.getEmail());
-        System.out.println("Found by Email: " + found);
-    }
-
-    @Test
-    @Order(5)
-    void findAccountByRegistrationDate() {
-        List<Account> accounts = accountService.findAccountByRegistrationDate(REG_DATE);
-        assertNotNull(accounts);
-        assertFalse(accounts.isEmpty());
-        System.out.println("Found by Registration Date: " + accounts);
-    }
-
-    @Test
-    @Order(6)
-    void getAll() {
-        List<Account> all = accountService.getAll();
-        assertNotNull(all);
-        assertFalse(all.isEmpty());
-        System.out.println("All Accounts: " + all);
-    }
-
-    @Test
-    @Order(7)
+    @Disabled
     void delete() {
         accountService.delete(account.getAccountId());
         Account read = accountService.read(account.getAccountId());
         assertNull(read);
-        System.out.println("Deleted account with ID: " + account.getAccountId());
+    }
+
+    @Test
+    @Order(4)
+    void getAll() {
+        List<Account> accounts = accountService.getAll();
+        System.out.println(accounts);
+    }
+
+    @Test
+    @Order(5)
+    void findByEmail() {
+        Account account1 = accountService.findByEmail("athi.sintiya@test.com");
+        System.out.println(account1);
+    }
+
+    @Test
+    @Order(6)
+    void findAccountByRegistrationDate() {
+        List<Account> accounts = accountService.findAccountByRegistrationDate(LocalDate.of(2026, 8, 20));
+        System.out.println(accounts);
     }
 }
