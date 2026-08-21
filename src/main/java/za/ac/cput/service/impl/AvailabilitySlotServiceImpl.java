2f8f0692fb8c4a177d3547eb7d82b635a46e8bf3
Phumelela Sakie <240040546@mycput.ac.za>
2026-08-21 14:57:56 +0200

    AvailabilitySlotServiceImpl Updated

--- src/main/java/za/ac/cput/service/impl/AvailabilitySlotServiceImpl.java
+++ src/main/java/za/ac/cput/service/impl/AvailabilitySlotServiceImpl.java
@@ -1,4 +1,47 @@
 package za.ac.cput.service.impl;
 
-public class AvailabilitySlotServiceImpl {
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.AvailabilitySlot;
+import za.ac.cput.repository.IAvailabilitySlotRepository;
+import za.ac.cput.service.IAvailabilitySlotService;
+
+import java.util.List;
+
+@Service
+public class AvailabilitySlotServiceImpl implements IAvailabilitySlotService {
+
+    private final IAvailabilitySlotRepository repository;
+
+    public AvailabilitySlotServiceImpl(IAvailabilitySlotRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public AvailabilitySlot create(AvailabilitySlot availabilitySlot) {
+        return repository.save(availabilitySlot);
+    }
+
+    @Override
+    public AvailabilitySlot read(String slotId) {
+        return repository.findById(slotId).orElse(null);
+    }
+
+    @Override
+    public List<AvailabilitySlot> readAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public AvailabilitySlot update(AvailabilitySlot availabilitySlot) {
+        return repository.save(availabilitySlot);
+    }
+
+    @Override
+    public boolean delete(String slotId) {
+        if (repository.existsById(slotId)) {
+            repository.deleteById(slotId);
+            return true;
+        }
+        return false;
+    }
 }
