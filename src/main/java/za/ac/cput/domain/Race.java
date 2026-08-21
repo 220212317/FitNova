f9d48e73220b5fe9a1f84b1aa4cda809b6b942b9
IngaPlati <230126634@mycput.ac.za>
2026-08-21 22:20:05 +0200

    Ignore Hibernate proxy fields when serializing Race to JSON

--- src/main/java/za/ac/cput/domain/Race.java
+++ src/main/java/za/ac/cput/domain/Race.java
@@ -1,13 +1,17 @@
-/* Race.java
-   Author: Inga Plati (230126634)
-   Date: 18 August 2026 */
+/*
+ * Race.java
+ * Author: Inga Plati
+ * 230126634
+ */
 package za.ac.cput.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Race {
 
     @Id
