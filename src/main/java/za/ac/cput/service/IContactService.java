88c2374df65ed950baa9a49565172733683beeec
Lisakhanyatshokolo5 <lisakhanyatshokolo@gmail.com>
2026-08-21 23:59:48 +0200

    Fixed contact service

--- src/main/java/za/ac/cput/service/IContactService.java
+++ src/main/java/za/ac/cput/service/IContactService.java
@@ -1,9 +1,9 @@
 package za.ac.cput.service;
 import za.ac.cput.domain.Contact;
-import java.util.Optional;
 //Lisakhanya Tshokolo 220239215
 
 public interface IContactService extends IService<Contact,String> {
     Contact findByCellphoneNumber(String cellphoneNumber);
     Contact findByEmailAddress(String emailAddress);
+    //Contact findByUser_UserId(String userId);
 }
