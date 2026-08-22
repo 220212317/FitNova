fe8837db7d4f2a41ae4c954caa1883c8332a9d9e
Lisakhanyatshokolo5 <lisakhanyatshokolo@gmail.com>
2026-08-22 20:33:03 +0200

    Modified Nextofkincontact Repository

--- src/main/java/za/ac/cput/repository/INextOfKinContactRepository.java
+++ src/main/java/za/ac/cput/repository/INextOfKinContactRepository.java
@@ -1,14 +1,15 @@
 package za.ac.cput.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import za.ac.cput.domain.NextOfKinContact;
+import za.ac.cput.domain.User;
 
 import java.util.List;
 import java.util.Optional;
 //Lisakhanya Tshokolo 220239215
 
 public interface INextOfKinContactRepository extends JpaRepository<NextOfKinContact, String> {
-    NextOfKinContact findByfirstName(String firstName);
-    NextOfKinContact findBylastName(String lastName);
-    NextOfKinContact findByrelationship(String relationship);
-    //List<NextOfKinContact> findByUser_UserId(String userId);
+    NextOfKinContact findByFirstName(String firstName);
+    NextOfKinContact findByLastName(String lastName);
+    NextOfKinContact findByRelationship(String relationship);
+    List<NextOfKinContact> findByUser(User user);
 }
