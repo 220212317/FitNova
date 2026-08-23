4f7b2645f887adda8569c3bc4769b65fc74fc98a
IngaPlati <230126634@mycput.ac.za>
2026-08-24 00:13:45 +0200

    Use ResponseEntity with proper status codes in GenderController

--- src/main/java/za/ac/cput/controller/GenderController.java
+++ src/main/java/za/ac/cput/controller/GenderController.java
@@ -1,57 +1,70 @@
-/*
- * GenderController.java
- * Author: Inga Plati
- * 230126634
- */
 package za.ac.cput.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import za.ac.cput.domain.Gender;
 import za.ac.cput.service.IGenderService;
 
 import java.util.List;
 
+/*
+ * GenderController.java
+ * Author: Inga Plati
+ * 230126634
+ */
+
 @RestController
 @RequestMapping("/gender")
 public class GenderController {
 
-    private final IGenderService service;
+    private final IGenderService genderService;
 
     @Autowired
-    public GenderController(IGenderService service) {
-        this.service = service;
+    public GenderController(IGenderService genderService) {
+        this.genderService = genderService;
     }
 
     @PostMapping("/create")
-    public Gender create(@RequestBody Gender gender) {
-        return service.create(gender);
+    public ResponseEntity<Gender> create(@RequestBody Gender gender) {
+        Gender created = genderService.create(gender);
+        if (created == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
     @GetMapping("/read/{genderId}")
-    public Gender read(@PathVariable String genderId) {
-        return service.read(genderId);
+    public ResponseEntity<Gender> read(@PathVariable String genderId) {
+        Gender gender = genderService.read(genderId);
+        if (gender == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(gender);
     }
 
     @PutMapping("/update")
-    public Gender update(@RequestBody Gender gender) {
-        return service.update(gender);
+    public ResponseEntity<Gender> update(@RequestBody Gender gender) {
+        Gender updated = genderService.update(gender);
+        if (updated == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(updated);
     }
 
     @DeleteMapping("/delete/{genderId}")
-    public void delete(@PathVariable String genderId) {
-        service.delete(genderId);
+    public ResponseEntity<Void> delete(@PathVariable String genderId) {
+        boolean deleted = genderService.delete(genderId);
+        if (!deleted) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/getAll")
-    public List<Gender> getAll() {
-        return service.getAll();
+    public ResponseEntity<List<Gender>> getAll() {
+        List<Gender> genders = genderService.getAll();
+        return ResponseEntity.ok(genders);
     }
 }
