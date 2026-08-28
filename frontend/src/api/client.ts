c7f7a8cbf7ebdbb3def428cce8f8edc2be5a11f6
Athi Sintiya <220212317@mycput.ac.za>
2026-08-28 20:29:16 +0200

    add exports

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,1 +1,18 @@
+import type {SetStateAction} from "react";
+
 /** TODO — Athi Sintiya (220212317) */
+export class ApiError {
+    message: SetStateAction<string | null> | undefined;
+}
+
+export class get {
+}
+
+export class post {
+}
+
+export class put {
+}
+
+export class del {
+}
