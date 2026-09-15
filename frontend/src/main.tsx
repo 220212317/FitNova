4128577049f5b146fd30639ed6b578b034cd49e3
Athi Sintiya <220212317@mycput.ac.za>
2026-09-15 22:04:02 +0200

    updated main to render

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./pages/dashboard.css";
+import "./pages/users.css";
+import "./pages/admin-shared.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
